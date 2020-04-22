56c19f3b8065c11182421987b9ecd7bcdb667de8
Theouty <aman.david.michael@gmail.com>
2020-04-22 14:45:24 -0700

    some message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function(e){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+
+    .then(json => {json.message
+    let s = document.getElementById('dog-image-container')
+    
+    let q = json.message 
+    for(let i =0; i<q.length; i++){
+        let a = document.createElement('img')
+        a.src= q[i];
+        s.appendChild(a);
+    }
+    
+})
+
+})
